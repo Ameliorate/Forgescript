04f6cfa59d16ba7b691ab3fa81fea0506565bf84
Ameliorate <ameliorate2@gmail.com>
2015-06-22 22:18:42 -0400

    Adds a block API
    Basically instead of using org.ame.jsforge.blockapi.JSBlockWrapper you just can use Block. Also a few other shortcuts for things in the blockapi namespace.

---
+++ src/main/resources/api/block.js
@@ -0,0 +1,4 @@
+var Block = Java.type("org.ame.jsforge.blockapi.JSBlockWrapper");
+var Material = Java.type("org.ame.jsforge.blockapi.MaterialWrapper");
+var RenderType = Java.type("org.ame.jsforge.blockapi.RenderType");
+var CreativeTab = Java.type("org.ame.jsforge.blockapi.CreativeTabsForgeScript");
