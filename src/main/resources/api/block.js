f327c459086a40f9182d84fc6137a78d815d3817
Ameliorate <ameliorate2@gmail.com>
2015-06-23 22:40:26 -0400

    Fixes an error where the api was using the old name for jsblock

--- src/main/resources/api/block.js
+++ src/main/resources/api/block.js
@@ -1,4 +1,4 @@
-var Block = Java.type("org.ame.jsforge.blockapi.JSBlockWrapper");
+var Block = Java.type("org.ame.jsforge.blockapi.JSBlock");
 var Material = Java.type("org.ame.jsforge.blockapi.MaterialWrapper");
 var RenderType = Java.type("org.ame.jsforge.blockapi.RenderType");
 var CreativeTab = Java.type("org.ame.jsforge.blockapi.CreativeTabsForgeScript");
